aac89f74a1b1a8043609d923ecc3992bfb7bf871
Maedoster <38166508+Maedoster@users.noreply.github.com>
2023-07-18 21:39:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,59 @@
 import telebot
 import sqlite3
+import string
+import random
 
 # Inserisci qui il token del tuo bot ottenuto da BotFather
 TOKEN = "5906905240:AAGSH8itptIwVd4NgAiQuMa-lDic2ZRE2kM"
 
-
 bot = telebot.TeleBot(TOKEN)
+
+# Funzione per generare numeri casuali composti da 6 cifre
+def generate_random_number():
+    return ''.join(random.choices(string.digits, k=6))
 
 #CREAZIONE DATABASE
 connection = sqlite3.connect('database_tg.db' ,check_same_thread=False)
-print(connection.total_changes)
 cursor = connection.cursor()
-#cursor.execute("CREATE TABLE freshman (name TEXT, surname TEXT, matricola )")
-cursor.execute("INSERT INTO freshman VALUES ('Luca', 'Capuccini', 347711)")
-cursor.execute("INSERT INTO freshman VALUES ('Chiara', 'Lombardo', 330350)")
-cursor.execute("INSERT INTO freshman VALUES ('Edoardo', 'Tommasi', 342244)")
-cursor.execute("INSERT INTO freshman VALUES ('Eduard', 'Brahas', 342600)")
-cursor.execute("INSERT INTO freshman VALUES ('Daniel', 'Chionne', 344785)")
 
+# Genera 1000 numeri casuali
+matricole = [generate_random_number() for _ in range(1000)]
+
+# Salva le matricole nel database come una lista di valori separati da virgola
+cursor.execute("CREATE TABLE IF NOT EXISTS freshman (matricole TEXT)")
+cursor.execute("INSERT INTO freshman (matricole) VALUES (?)", (",".join(matricole),))
+connection.commit()
+
+def check_matricola(message):
+    matricola_numero = message.text
+
+    if not matricola_numero.isdigit() or len(matricola_numero) != 6:
+        bot.reply_to(message, "⚠️ La matricola deve essere composta da 6 cifre numeriche. \nInserisci nuovamente la matricola:")
+        bot.register_next_step_handler(message, check_matricola)
+        return
+
+    cursor.execute("SELECT matricole FROM freshman")
+    matricole = cursor.fetchone()[0].split(",")
+
+    if matricola_numero in matricole:
+        bot.reply_to(message, f"✅ La matricola è corretta {message.from_user.first_name}!")
+        bot.send_message(message.chat.id, "Ora puoi utilizzare i seguenti comandi:")
+        bot.send_message(message.chat.id, "/start - Per reinserire la matricola")
+        bot.send_message(message.chat.id, "/lista_esami - Per vedere lista degli esami prenotabili")
+        bot.send_message(message.chat.id, "/prenotazione_esami - Per prenotare esami")
+    else:
+        bot.reply_to(message, "❌ La matricola è incorretta. \nInserisci nuovamente la matricola:")
+        bot.register_next_step_handler(message, check_matricola)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.reply_to(message, 'inserisci la matricola:')
+    bot.reply_to(message, f"👋 Benvenuto in ExamBot {message.from_user.first_name}!\n Per iniziare, inserisci la tua matricola:")
 
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    matricola_numero = int(message.text)
-    bot.reply_to(message, f"La tua matricola è {matricola_numero}.")
-    query = f"SELECT matricola FROM freshman WHERE matricola = {matricola_numero}"
-    cursor.execute(query)
-    risultato = cursor.fetchone
-    connection.commit()
-
-    if risultato:
-        print("Il valore è presente nel database DIO CANE.")
-    else:
-        print("Il valore non è presente nel database.")
+    # Avvia la funzione di controllo della matricola
+    check_matricola(message)
 
 
 bot.polling()
-
